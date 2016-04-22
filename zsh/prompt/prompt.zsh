afb87b83f0b2259ce5bd5298b7afa31ea6db0400
Rob Kennedy <kennedyri@gmail.com>
2016-04-22 06:28:06 -0700

    Begin workings of native Zsh Powerline-style prompt

---
+++ zsh/prompt/prompt.zsh
@@ -0,0 +1,177 @@
+# Prompt:
+# On left:
+# - remote host name, plus SSH indicator: ${(%):-%m}, plus ?
+# - current user name: ${(%):-%n}
+# - abbreviated path: ${(%):-%3~}
+# - number of background jobs: ${(%):-%j}
+#
+# On right:
+# - results of previous command pipeline: ${pipestatus[@]}
+# - current branch or commit ID, if in source repo
+zmodload zsh/zutil
+setopt prompt_subst
+
+autoload colors && colors
+
+zstyle :prompts:rkline security-indicator ''
+zstyle :prompts:rkline section-separator ''
+
+# Prompt function should set $reply array to contain the prompt contents,
+# the starting background color, and the ending background color. It should
+# set it to empty if there should be no contents for the prompt section.
+zstyle ':prompts:*:prompt_hostname' fg '220'
+zstyle ':prompts:*:prompt_hostname' bg '166'
+function prompt_hostname()
+{
+    reply=()
+    if (($+SSH_CLIENT)); then
+        zstyle -g secure_char :prompts:rkline security-indicator
+        zstyle -g fgc ':prompts:*:prompt_hostname' fg
+        zstyle -g bgc ':prompts:*:prompt_hostname' bg
+        reply+=("%{%F{$fgc}%K{$bgc}%}${secure_char} %m%{%f%k%}")
+        reply+=($bgc $bgc)
+    fi
+}
+zstyle ':prompts:*:prompt_user' fg 'white'  # also bold
+zstyle ':prompts:*:prompt_user' bg '31'
+function prompt_user()
+{
+    reply=()
+    zstyle -g fgc ':prompts:*:prompt_user' fg
+    zstyle -g bgc ':prompts:*:prompt_user' bg
+    reply+=("%{%F{$fgc}%K{$bgc}%}%n%{%f%k%}")
+    reply+=($bgc $bgc)
+}
+zstyle ':prompts:*:prompt_cwd' fg '250'  # last component also bold
+zstyle ':prompts:*:prompt_cwd' bg '240'
+zstyle ':prompts:rkline:prompt_cwd' ps '  '
+zstyle ':prompts:rkline' elipsis '⋯'
+function prompt_cwd()
+{
+    reply=()
+    zstyle -g fgc ':prompts:*:prompt_cwd' fg
+    zstyle -g bgc ':prompts:*:prompt_cwd' bg
+    cwd=${(%):-%3~}
+    local -a result
+    if [[ $cwd == '/' ]]; then
+        result+=('/')
+    else
+        if [[ $cwd =~ '^/' ]]; then
+            # path is absolute
+            result+=('/')
+            cwd="${cwd[2,-1]}"
+        elif [[ $cwd =~ '^~' ]]; then
+            # path isn't truncated
+        else
+            zstyle -s ':prompts:rkline' elipsis elipsis
+            result+=($elipsis)
+        fi
+        result+=("${(q@s:/:)cwd}")
+    fi
+    zstyle -s ':prompts:rkline:prompt_cwd' ps ps
+    reply+=("%{%F{$fgc}%K{$bgc}%}${(ej:${ps}:)result}%{%f%k%}")
+    reply+=($bgc $bgc)
+}
+zstyle ':prompts:*:prompt_jobs' fg '220'
+zstyle ':prompts:*:prompt_jobs' bg '166'
+function prompt_jobs()
+{
+    reply=()
+    if ((${(%):-%j} > 0)); then
+        zstyle -g fgc ':prompts:*:prompt_jobs' fg
+        zstyle -g bgc ':prompts:*:prompt_jobs' bg
+        reply+=("%{%F{$fgc}%K{$bgc}%}%j%{%f%k%}")
+        reply+=($bgc $bgc)
+    fi
+}
+zstyle ':prompts:*:prompt_pipestatus:pass' fg 'white'
+zstyle ':prompts:*:prompt_pipestatus:fail' fg 'white'
+zstyle ':prompts:*:prompt_pipestatus:pass' bg 'green'
+zstyle ':prompts:*:prompt_pipestatus:fail' bg 'dark red'
+zstyle ':prompts:*:prompt_pipestatus' separator '|'
+function prompt_pipestatus()
+{
+    ps=($pipestatus)
+    reply=()
+    success=(0)
+    if [[ ${#ps:*success} -ne ${#ps} ]]; then
+        zstyle -g sep ':prompts:*:prompt_pipestatus' separator
+        first=0
+        for s in $ps; do
+            if [[ $s -eq 0 ]]; then
+                zstyle -g fgc 'prompts:*:prompt_pipestatus:pass' fg
+                zstyle -g bgc 'prompts:*:prompt_pipestatus:pass' bg
+            else
+                zstyle -g fgc 'prompts:*:prompt_pipestatus:fail' fg
+                zstyle -g bgc 'prompts:*:prompt_pipestatus:fail' bg
+            fi
+            if [[ $first -ne 0 ]]; then
+                first=1
+                result+=" %{%F{$bgc}%}$sep%{%F{$fgc}%K{$bgc}%} $s"
+            else
+                result+="%{%F{$fgc}%K{$bgc}%}${s}"
+                first_bgc=$bgc
+            fi
+        done
+        result+="%{%f%k$}"
+        reply+=($result)
+        reply+=($first_bgc $bgc)
+    fi
+    print $reply
+}
+zstyle ':prompts:*:prompt_git_commit' fg 'light gray'
+zstyle ':prompts:*:prompt_git_commit' bg 'dark gray'
+zstyle ':prompts:rkline' branch ''
+function prompt_git_commit()
+{
+    reply=()
+    ID=$(git symbolic-ref --quiet --short HEAD || git describe --all --exact-match 2>|/dev/null || git rev-parse --short HEAD)
+    if $?; then
+        zstyle -g fgc ':prompts:*:prompt_git_commit' fg
+        zstyle -g bgc ':prompts:*:prompt_git_commit' bg
+        zstyle -g BRANCH ':prompts:rkline' branch
+        reply+=("%{%F{$fgc}%K{$bgc}%}${BRANCH} ${ID}%{%f%k%}")
+        reply+=($bgc $bgc)
+    fi
+}
+
+typeset -a ps1_functions
+typeset -a rps1_functions
+
+ps1_functions+=(prompt_hostname)
+ps1_functions+=(prompt_user)
+ps1_functions+=(prompt_cwd)
+#ps1_functions+=(prompt_jobs)
+#rps1_functions+=(prompt_pipestatus)
+rps1_functions+=(prompt_git_commit)
+
+function do_prompt()
+{
+    prompt_pipestatus=$pipestatus
+    if [[ $1 == "right" ]]; then
+        fns=(${rps1_functions})
+    else
+        fns=(${ps1_functions})
+    fi
+    zstyle -s ':prompts:rkline' section-separator sep
+    local first=1
+    local result
+    for func in ${(@)fns}; do
+        $func
+        text=${reply[1]}
+        start_bgc=${reply[2]}
+        end_bgc=${reply[3]}
+        if [ $first -eq 0 ]; then
+            result+="%{%K{${start_bgc}}%}${sep}%{%f%}"
+        else
+            result+="%{%F{${old_end_bgc}}%K{${start_bgc}}%}"
+            first=0
+        fi
+        result+="%{%K{${start_bgc}}%} %{%k%}${text}%{%K{${end_bgc}}%} %{%k%}"
+        result+="%{%F{${end_bgc}}%}"
+    done
+    result+="%{%k%}$sep%{%f%} "
+    print -n $result
+}
+
+PS1='$(do_prompt left)'
